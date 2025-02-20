244441a3e6ff3cfcb5d6eaa683ef67e9c4cffa09
hjkim <hjhjgudwls1022@gmail.com>
2025-02-20 16:25:27 +0900

    chore: 초기 프로젝트 셋업

---
+++ src/main/java/com/doneflow/DoneFlowApplication.java
@@ -0,0 +1,13 @@
+package com.doneflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoneFlowApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DoneFlowApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/doneflow/DoneFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doneflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoneFlowApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
