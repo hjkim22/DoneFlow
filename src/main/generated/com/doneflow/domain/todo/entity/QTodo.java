11ea70ed68846946d912e5aab9fb1e9308903542
hjkim <hjhjgudwls1022@gmail.com>
2025-02-24 17:35:24 +0900

    feat: Q클래스 생성 환경 구성(Hibernate Dialect 명시적 설정 추가 (MySQL8Dialect))

---
+++ src/main/generated/com/doneflow/domain/todo/entity/QTodo.java
@@ -0,0 +1,67 @@
+package com.doneflow.domain.todo.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QTodo is a Querydsl query type for Todo
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QTodo extends EntityPathBase<Todo> {
+
+    private static final long serialVersionUID = -238213022L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QTodo todo = new QTodo("todo");
+
+    public final com.doneflow.common.base.QBaseTimeEntity _super = new com.doneflow.common.base.QBaseTimeEntity(this);
+
+    public final com.doneflow.domain.category.entity.QCategory category;
+
+    public final BooleanPath completed = createBoolean("completed");
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final DateTimePath<java.time.LocalDateTime> dueDate = createDateTime("dueDate", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath title = createString("title");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QTodo(String variable) {
+        this(Todo.class, forVariable(variable), INITS);
+    }
+
+    public QTodo(Path<? extends Todo> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QTodo(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QTodo(PathMetadata metadata, PathInits inits) {
+        this(Todo.class, metadata, inits);
+    }
+
+    public QTodo(Class<? extends Todo> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.category = inits.isInitialized("category") ? new com.doneflow.domain.category.entity.QCategory(forProperty("category")) : null;
+    }
+
+}
+
