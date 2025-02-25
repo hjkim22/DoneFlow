87603afdd15696c7bdb71207049964606f0c7ab8
hjkim <hjhjgudwls1022@gmail.com>
2025-02-26 00:23:10 +0900

    feat: UserRepository 생성 - User 엔티티용 Repository 추가 (findByEmail) - Todo 엔티티용 Repository 추가 (findByUser)

--- src/main/java/com/doneflow/domain/todo/repository/TodoRepository.java
+++ src/main/java/com/doneflow/domain/todo/repository/TodoRepository.java
@@ -1,10 +1,13 @@
 package com.doneflow.domain.todo.repository;
 
 import com.doneflow.domain.todo.entity.Todo;
+import com.doneflow.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
+  List<Todo> findByUser(User user);
 }
